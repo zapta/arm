b0b817208a762f75e79ddbedf7fbedf5159df68e
Zapta <zapta@zapta.com>
2014-12-10 08:54:32 -0800

    Added two precompiled hello world binaries for the Getting Started procedure.

--- pro-mini/lpcxpresso/hello_world/src/hello_world.cpp
+++ pro-mini/lpcxpresso/hello_world/src/hello_world.cpp
@@ -1,12 +1,12 @@
-// A basic hello world example using the ezlpc library. Text output is sent to
-// the USB serial port (CDC virtual com). 
+// A basic hello world example using the arm_pro_mini library. It blinks the LED
+// and prints to the USB/CDC serial port.
 
 #include "arm_pro_mini.h"
 
 // Provides abstraction to digital I/o pins.
 #include "io_pins.h"
 
-// Provide system time using timer xxx.
+// Provide system time using TIMER32 0.
 #include "system_time.h"
 
 // Provides interrupt free elapsed time measurement using system time.
@@ -17,6 +17,13 @@
 
 // Allows to jump to ISP mode when ISP button is pressed.
 #include "isp_button_monitor.h"
+
+// LED blink cycle. We provide two prebuilt binaries with
+// fast and slow blink respectively to be used in the
+// Getting Started procedure.
+//
+static const uint32 kCycleTimeUsecs = 300 * 1000;  // fast
+// static const uint32 kCycleTimeUsecs = 2000 * 1000;  // slow
 
 // Timer for generating the delay bettween printed messages.
 static PassiveTimer timer;
@@ -30,32 +37,29 @@ static void setup() {
   system_time::setup();
   // Initialize the USB serial connection. This will allow us to print messages.
   usb_serial::setup();
-  
   // Get ready to monitor the ISP button
   isp_button_monitor::setup();
-
   // Reset the timer to the time now. This starts the first cycle.
   timer.reset();
 }
 
 static void loop() {
+  // If the ISP button is pressed, this will jump to the USB/ISP
+  // mode, allowing to upgrade the firmware via drag and drop.
   isp_button_monitor::loop();
 
   static int message_count = 0;
+  const uint32 time_now_in_cycle_usecs = timer.usecs();
 
-  const uint32 time_now_usecs = timer.usecs();
-
-  const uint32 cycle_time_usecs = 100000;
-
-  // Generates a short blink at the beginning of each cycle.
-  led.set(time_now_usecs <= cycle_time_usecs / 3);
-  if (time_now_usecs >= cycle_time_usecs) {
+  // Generates a blink at the beginning of each cycle.
+  led.set(time_now_in_cycle_usecs <= kCycleTimeUsecs / 3);
+  if (time_now_in_cycle_usecs >= kCycleTimeUsecs) {
     usb_serial::printf("Hello world: %d, %u\n", message_count,
         system_time::usecs());
     message_count++;
     // Advance cycle start time rather than reseting to time now. This
     // way we don't accumulate time errors.
-    timer.advance_start_time_usecs(cycle_time_usecs);
+    timer.advance_start_time_usecs(kCycleTimeUsecs);
   }
 }
 
