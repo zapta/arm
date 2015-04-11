b37fe56ec69f83bafe5f53aa1bf173e5b159fcbb
Zapta <zapta@zapta.com>
2015-04-11 09:03:25 -0700

    Updated self tester for the nee LED jig.

--- pro-mini/lpcxpresso/self_test/src/self_test.cpp
+++ pro-mini/lpcxpresso/self_test/src/self_test.cpp
@@ -1,23 +1,10 @@
 // A production test program. Each output pin generates pulses
 // whose period in milliseconds equals the pin number.
 
-// The base arm_pro_mini_lib include.
 #include "arm_pro_mini.h"
-
-// Provides abstraction to digital I/o pins.
 #include "io_pins.h"
-
-// Provide system time using TIMER32 0.
 #include "system_time.h"
-
-// Provides interrupt free elapsed time measurement using system time.
 #include "passive_timer.h"
-
-// Allows to jump to ISP mode when ISP button is pressed.
-#include "isp_button_monitor.h"
-
-// Timer for timing the cycles.
-static PassiveTimer timer;
 
 // Extends an output pin with a physical pin number.
 class NumberedOutputPin : public io_pins::OutputPin {
@@ -30,7 +17,7 @@ public:
 
 // Output pins to test. Each pins generates a pulse whose width
 // in milliseconds is the physical pin number.
-static NumberedOutputPin pinTable[] = {
+static NumberedOutputPin pin_table[] = {
   NumberedOutputPin(1, 0, 21),
   NumberedOutputPin(2, 0, 6),
   // LED on early versions boards.
@@ -65,37 +52,28 @@ static NumberedOutputPin pinTable[] = {
   NumberedOutputPin(26, 0, 5),
 };
 
-static const int kPinCount =  sizeof(pinTable) / sizeof(pinTable[0]);
+static const int kPinCount =  sizeof(pin_table) / sizeof(pin_table[0]);
+
+static int current_pin_index;
+
+static PassiveTimer timer;
 
 static void setup() {
   arm_pro_mini::setup();
-  // Uses timer32_0 for generating a 1 usec 32 bit clock (does not use interrupts).
   system_time::setup();
-  // Get ready to monitor the ISP button
-  isp_button_monitor::setup();
-  // Reset the timer to the time now. This starts the first cycle.
   timer.reset();
+  current_pin_index = 0;
 }
 
 static void loop() {
-  // If the ISP button is pressed, this will jump to the USB/ISP
-  // mode, allowing to upgrade the firmware via drag and drop.
-  // Otherwise just reset the board while the ISP button is pressed.
-  isp_button_monitor::loop();
-
-  const uint32 time_now_in_cycle_usecs = timer.usecs();
-  const uint32 time_now_in_cycle_msecs = time_now_in_cycle_usecs / 1000;
-
-  // Update all pins based on their pin number and the time in current
-  // cycle.
   for (int i = 0; i < kPinCount; i++) {
-    NumberedOutputPin& pin = pinTable[i];
-    pin.set(pin.pin_number > time_now_in_cycle_msecs);
+    // Pins are arcive low.
+    pin_table[i].set(i != current_pin_index);
   }
 
-  // Check if current cycle done.
-  if (time_now_in_cycle_msecs >= 50) {
+  if (timer.usecs() >= 100000) {
     timer.reset();
+    current_pin_index = (current_pin_index + 1) % kPinCount;
   }
 }
 
