4adbd943e408fadad262af00f0353dbccda18d10
Zapta <zapta@zapta.com>
2016-07-22 09:51:06 -0700

    Reformatted

--- pro-mini/lpcxpresso/mbed_hello_world/src/main.cpp
+++ pro-mini/lpcxpresso/mbed_hello_world/src/main.cpp
@@ -24,33 +24,33 @@ USBSerial usb_serial(0x1f00, 0x2012, 0x0001, false);
 AnalogIn analog_in(P0_11);
 
 static void setup() {
-	timer.start();
-	sys_time.start();
+  timer.start();
+  sys_time.start();
 }
 
 static void loop() {
-	static int message_count = 0;
-	const uint32_t time_now_in_cycle_msecs = timer.read_ms();
+  static int message_count = 0;
+  const uint32_t time_now_in_cycle_msecs = timer.read_ms();
 
-	// Generates a blink at the beginning of each cycle.
-	const bool led_state = time_now_in_cycle_msecs <= kCycleTimeMsecs / 3;
-	led1 = led_state;
-	legacy_led = led_state;
+  // Generates a blink at the beginning of each cycle.
+  const bool led_state = time_now_in_cycle_msecs <= kCycleTimeMsecs / 3;
+  led1 = led_state;
+  legacy_led = led_state;
 
-	if (time_now_in_cycle_msecs >= kCycleTimeMsecs) {
-		uint16_t analog_level = analog_in.read_u16();
-		// NOTE: using \r\n EOL for the benefit of dumb serial dump. Typically
-		// \n is sufficient.
-		usb_serial.printf("Hello world: %d, ADC0: %u, %u\r\n", message_count,
-				analog_level, sys_time.read_ms());
-		message_count++;
-		timer.reset();
-	}
+  if (time_now_in_cycle_msecs >= kCycleTimeMsecs) {
+    uint16_t analog_level = analog_in.read_u16();
+    // NOTE: using \r\n EOL for the benefit of dumb serial dump. Typically
+    // \n is sufficient.
+    usb_serial.printf("Hello world: %d, ADC0: %u, %u\r\n", message_count,
+        analog_level, sys_time.read_ms());
+    message_count++;
+    timer.reset();
+  }
 }
 
 int main(void) {
-	setup();
-	for (;;) {
-		loop();
-	}
+  setup();
+  for (;;) {
+    loop();
+  }
 }
