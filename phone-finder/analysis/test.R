1999b59bd8578e25d73ef21db7baaf0efe92f193
Zapta <zapta@zapta.com>
2016-08-26 15:22:16 -0700

    Calibrated the time units

--- phone-finder/analysis/test.R
+++ phone-finder/analysis/test.R
@@ -1,20 +1,34 @@
 #!/usr/local/bin/Rscript
 
+f1 = 700
+f2 = 1600
 
-xs <- seq(-2*pi,2*pi,pi/100)
+# Sampling frequency HZ
+sf <- 100000
+paste("Sampling freq :", sf, "[HZ]")
 
-wave.1 <- sin(3*xs)
-wave.2 <- sin(10*xs)
+# Total sampling period secs
+sp <- 0.01
+paste("Total sampling period :", sp, "[sec]")
 
+# Sampling interval secs
+si <- 1/ sf
+paste("Sampling interval :", si, "[sec]")
+
+# Generate sampling time series in sec
+ts <- seq(0, sp, si)
+
+wave.1 <- sin(f1*2*pi*ts)
+wave.2 <- sin(f2*2*pi*ts)
 
 X11(width=10, height=8)
 
 par(mfrow = c(3, 1))
 
-plot(xs,wave.1, type="l", ylim=c(-1,1)) 
+plot(ts, wave.1, type="l", ylim=c(-1,1)) 
 abline(h=0,lty=3)
 
-plot(xs,wave.2, type="l", ylim=c(-1,1)) 
+plot(ts,wave.2, type="l", ylim=c(-1,1)) 
 abline(h=0,lty=3)
 
 message("Press return to exit...")
